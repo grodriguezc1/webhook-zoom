73f94185b3d647d434be545dd8f6ceed83b7822b
Gabriel Rodriguez <34773274+grodriguezc1@users.noreply.github.com>
2025-09-03 10:43:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
